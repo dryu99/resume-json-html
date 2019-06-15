b1070360b2aad13494a45115428d127839adba82
Daniel Ryu <rujjiryu99@gmail.com>
2019-06-15 03:06:58 -0700

    add reset.css file, start basic rendering of resume header

--- logic.js
+++ logic.js
@@ -2,9 +2,9 @@
 
 // Data Controller module
 const dataController = (() => {
-	const resume = data;
+	const _resume = data; // parsing would happen here
 
-	const getResume = () => resume;
+	const getResume = () => _resume;
 
 	/**
 	 * maybe make more functions to access each part of the resume? 
@@ -14,4 +14,34 @@ const dataController = (() => {
 
 })();
 
-console.log(dataController.getResume().header.name);
+
+// Display Controller module
+const displayController = (() => {
+	const _resume = dataController.getResume();
+
+	const render = () => {
+		_renderHeader();
+	};
+
+	const _renderHeader = () => {
+		const header = _resume.header; // maybe make a getHeader method in data control?
+
+		// use bootstrap for left, mid, right justified
+		$(".header").html(`
+			<div class="header-name">${header.name}</div>
+			<div class="header-title">${header.title}</div>
+			<div class="header-row-1">Ph: ${header.phone} ${header.websites.github} ${header.email}</div> 
+			<div class="header-row-2">${header.websites.github}</div>
+			`
+		);
+	}
+
+	const _renderTitle = () => {
+		
+	};
+	
+	return { render };
+})();
+
+displayController.render();
+
