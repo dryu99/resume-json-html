22be0704781ec6b41975214a0e60e2945382bb32
Daniel Ryu <rujjiryu99@gmail.com>
2019-06-15 02:12:05 -0700

    enter tentative data for resume.json

--- logic.js
+++ logic.js
@@ -1,11 +1,17 @@
-let resume;
 
-$.ajax({
-	url: "./resume.json",
-	method: "GET",
-	success: function (response) {
-		resume = JSON.parse(response);
-	}
-});
 
-console.log(resume.basics.name);
+// Data Controller module
+const dataController = (() => {
+	const resume = data;
+
+	const getResume = () => resume;
+
+	/**
+	 * maybe make more functions to access each part of the resume? 
+	 */
+
+	return { getResume };
+
+})();
+
+console.log(dataController.getResume().header.name);
