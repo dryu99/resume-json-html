a84836179e75dcfec58ae3064259bc6f930add59
Daniel Ryu <rujjiryu99@gmail.com>
2019-06-15 03:47:55 -0700

    implement basic html render of skills section

--- logic.js
+++ logic.js
@@ -14,6 +14,19 @@ const dataController = (() => {
 
 })();
 
+// Template module
+const template = (() => {
+	
+	const skillRow = (skill) => {
+		return `<div class="skill-row">
+							<b>${skill.type}</b>: ${skill.names.join(" | ")}
+						</div>`
+	};
+
+	return { skillRow };
+
+})();
+
 
 // Display Controller module
 const displayController = (() => {
@@ -21,27 +34,39 @@ const displayController = (() => {
 
 	const render = () => {
 		_renderHeader();
+		_renderSkills();
 	};
 
 	const _renderHeader = () => {
 		const header = _resume.header; // maybe make a getHeader method in data control?
 
-		// use bootstrap for left, mid, right justified
+		// use bootstrap for left, mid, right justified of rows 
 		$(".header").html(`
-			<div class="header-name">${header.name}</div>
-			<div class="header-title">${header.title}</div>
+			<div class="my-name">${header.name}</div>
+			<div class="my-title">${header.title}</div>
 			<div class="header-row-1">Ph: ${header.phone} ${header.websites.github} ${header.email}</div> 
 			<div class="header-row-2">${header.websites.github}</div>
 			`
 		);
 	}
 
-	const _renderTitle = () => {
-		
+	const _renderSkills = () => {
+		const skills = _resume.skills; 
+
+		$(".skills").html(`
+			${_renderTitle("SKILLS")}
+			${skills.map(template.skillRow).join("")} 			
+			<span class="footnote">*currently acquiring</span>
+			`
+		);
+	};
+
+	const _renderTitle = (title) => {
+		return `<div class="section-title">${title}</div>`;
 	};
 	
 	return { render };
 })();
 
 displayController.render();
-
+console.log($(".projects").index());
